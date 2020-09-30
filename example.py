117648381e2890d12858d29ef5bcc9cb073569a4
Alex Sinclair <amansinclair@gmail.com>
2020-09-30 10:38:25 +0200

    formatted

--- example.py
+++ example.py
@@ -1,13 +1,14 @@
 if __name__ == "__main__":
-    from gol import run, GOL, Blinker, Toad, Beacon, Ant, LWSS, Pulsar
+    from gol import run, GOL, PATTERNS
     import numpy as np
 
     size = 100
-    a = np.zeros((size, size), dtype="int")
-    items = [Blinker(), Toad(), Beacon(), Ant(), LWSS(), Pulsar()]
-    locs = [(20, 90), (30, 30), (70, 70), (10, 10), (20, 20), (70, 20)]
-    for loc, item in zip(locs, items):
-        item.add_to(a, *loc)
-    gol = GOL()
-    gol.set_board(a)
+    n_patterns = 10
+    board = np.zeros((size, size), dtype="int")
+    for i in range(n_patterns):
+        pattern in np.random.choice(PATTERNS)
+        row = np.random.choice(np.arange(size))
+        col = np.random.choice(np.arange(size))
+        pattern.add_to(board, row, col)
+    gol = GOL(board)
     run(gol)
--- gol/__init__.py
+++ gol/__init__.py
@@ -1,2 +1,2 @@
 from .app import run, GOL
-from .patterns import PATTERNS, Blinker, Toad, Beacon, Ant, LWSS, Pulsar, 
+from .patterns import PATTERNS, Blinker, Toad, Beacon, Ant, LWSS, Pulsar
