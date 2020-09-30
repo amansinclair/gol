3de3773c9f77cbe216cefb4fafcaec3a2e5a989d
Alex Sinclair <amansinclair@gmail.com>
2020-09-30 11:05:04 +0200

    refactored for random pixels

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ if __name__ == "__main__":
     from gol import run, GOL, PATTERNS
     import numpy as np
 
-    size = 50
-    n_patterns = 10
+    size = 100
+    n_patterns = 20
     board = np.zeros((size, size), dtype="int")
     for i in range(n_patterns):
         pattern = np.random.choice(PATTERNS)
@@ -11,4 +11,5 @@ if __name__ == "__main__":
         col = np.random.choice(np.arange(size))
         pattern.add_to(board, row, col)
     gol = GOL(board)
+    gol.add_random_cells(coverage=0.3)
     run(gol)
--- gol/app.py
+++ gol/app.py
@@ -10,35 +10,39 @@ WIDTH = 900
 class GOL:
     def __init__(self, board=None, size=None, coverage=None):
         if not hasattr(board, "shape"):
-            board = self.create_random_board(size, coverage)
-        self.set_board(board)
-
-    def set_board(self, board):
-        self.board = board
-        self.board[np.where(board > 0)] = 255
+            self.board = self.create_random_board(size, coverage)
+        else:
+            self.board = board
         self.scale = (HEIGHT - 100) // self.board.shape[0]
+        self.set_alive_to_white()
 
-    def scale_array(self, board):
-        return board.repeat(self.scale, axis=0).repeat(self.scale, axis=1)
+    def set_alive_to_white(self):
+        self.board[np.where(self.board > 0)] = 255
+
+    def scale_for_screen(self):
+        return self.board.repeat(self.scale, axis=0).repeat(self.scale, axis=1)
 
     def create_random_board(self, size=None, coverage=None):
         size = size if size else 20
         board = np.zeros((size, size), dtype="int")
-        coverage = coverage if coverage else int(0.8 * (size ** 2))
-        return add_random_cells(board, coverage)
+        coverage = coverage if coverage else 0.5
+        self.add_random_cells(coverage, board)
+        return board
 
-    def add_random_cells(self, board, coverage):
+    def add_random_cells(self, coverage, board=None):
+        if not hasattr(board, "shape"):
+            board = self.board
         size = board.shape[0]
+        coverage = int(coverage * (size ** 2))
         idxs = np.random.choice(np.arange(size ** 2), coverage)
         np.put(board, idxs, 255)
-        return board
 
     def update(self):
         board = self.board.copy()
         for row in range(self.board.shape[0]):
             for col in range(self.board.shape[1]):
                 self.board[row, col] = self.update_pixel(board, row, col)
-        return self.scale_array(self.board)
+        return self.scale_for_screen()
 
     def update_pixel(self, board, row, col):
         old_state = board[row, col]
@@ -72,7 +76,7 @@ def update(root, gol, canvas):
 
 def run(gol):
     root = tk.Tk()
-    start_array = gol.scale_array(gol.board)
+    start_array = gol.scale_for_screen()
     img = ImageTk.PhotoImage(image=Image.fromarray(start_array))
     x = (WIDTH - start_array.shape[0]) // 2
     y = (HEIGHT - start_array.shape[1]) // 2
